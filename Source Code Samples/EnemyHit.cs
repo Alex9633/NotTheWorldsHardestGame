ffebf474f1bf88914bacbd709b1d4064e512bc26
Alex9633 <120327803+Alex9633@users.noreply.github.com>
2024-02-20 23:39:54 +0100

    add source code samples

---
+++ Source Code Samples/EnemyHit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHit : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "enemy")
+        {
+            FindObjectOfType<AudioManager>().Play("Hit");
+            StartCoroutine(DieAndRespawn());
+        }
+    }
+
+    IEnumerator DieAndRespawn()
+    {
+        GetComponent<Renderer>().enabled = false;
+        yield return new WaitForSeconds(0.1f);
+        transform.position = new Vector3(-10.5f, -2.5f, 0.0f);
+        transform.rotation = Quaternion.identity;
+        GetComponent<Renderer>().enabled = true;
+    }
+}
+
---
+++ Source Code Samples/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(4, 4);
+
+    void Update()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
+        movement *= Time.deltaTime;
+        transform.Translate(movement);
+    }
+}
+
